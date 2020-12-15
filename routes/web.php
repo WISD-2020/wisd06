3c0f56578d25f30cd8ea2848685006d742671045
li ya hsuan <ab53014698@yahoo.com.tw>
2020-12-15 21:27:29 +0800

    建立HomeController & 路由aa.a1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', [HomeController::class,'home']);
+
+Route::get('/aa/a1', function (){
+    return view('aa.a1');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
